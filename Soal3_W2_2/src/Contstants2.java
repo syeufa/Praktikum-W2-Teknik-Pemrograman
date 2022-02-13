7a868b89a434d820bf239cc135829f1aa91182c8
syeufa <95170161+syeufa@users.noreply.github.com>
2022-02-13 16:06:58 +0700

    Praktikum W2 Teknik Pemrograman
    Studi Kasus Topik 2 - Fundamental Programming Structures
    Soal 1: Comments
    Soal 2: Data Types
    Soal 3: Variables
    Soal 4: Operators
    Soal 5: Operators (1)
    Soal 6: Strings

---
+++ Soal3_W2_2/src/Contstants2.java
@@ -0,0 +1,16 @@
+/**
+* @author Syifa Khairina
+* @version 1.0
+* @since 2022-02-10
+*/
+
+public class Contstants2 {
+ public static final double CM_PER_INCH = 2.54;
+ public static void main(String[] args) {
+ double paperWidth = 8.5;
+ double paperHeight = 11;
+ System.out.println("Paper size in centimeters: " + paperWidth *
+ CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
+ }
+}
+  
